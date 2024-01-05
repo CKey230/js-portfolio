02452f2b8e14573f45bdbad6b4e14a8681e0451a
Ckey230 <Ckey230@gmail.com>
2024-01-05 09:23:33 -0500

    rough draft components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './components/Home';
+import Project from './components/Project'
+import About from './components/About'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/project' element={<Project />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/Contact' element={<Contact />}/>
+            <Route path='/Resume' element={<Resume />}/>
+          </Routes>
+        </div>
+      </Router>
+     
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,7 @@
+export default function Resume(){
+    return(
+        <div>
+            <h1>this is my resume page</h1>
+        </div>
+    )
+}
